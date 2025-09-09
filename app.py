52ae11dcb365903cf643a3cb6dacf5678f8ad793
Darshik Italiya <dai.globalia@gmail.com>
2025-09-09 14:16:22 +0530

    add form validation

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
+from form import MyForm
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "rk4hi5jthn5tji56h6kj7nkjntjgkfng"
 
 @app.route("/")
 def home():
     item_list = ["python", "react", "node.js", "next.js", "websocket"]
     return render_template("index.html", items=item_list)
 
-@app.route("/about")
-def about():
-    return "About Page"
-
-
-@app.route("/user/<username>/<age>")
-def show_user(username, age):
-    return render_template("user.html", name=username, user_age=age)
-
-
-@app.route("/post/<int:post_id>", methods=["GET"])
-def show_post(post_id):
-    return "Post_ID: %s" % post_id
-
-
 @app.route("/submit", methods=["GET", "POST"])
 def submitData():
-    if request.method == "POST":
-        name = request.form.get("name")
-        age = request.form.get("age")
-        return f"Data Submitted Successfully.. name: {name}, age: {age}"
+    form = MyForm()
+    if form.validate_on_submit():
+        flash("Form submitted successfully...")
+        
+        return redirect(url_for("submitData"))
     else:
-        name = request.args.get("name")
-        age = request.args.get("age")
-        return f"Data Submitted Successfully.. name: {name}, age: {age}"
+       return render_template("form.html", form=form)
 
 
 if __name__ == "__main":
