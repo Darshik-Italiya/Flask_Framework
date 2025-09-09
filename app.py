8361751f2247d5b504f0be504d0991a79220663b
Darshik Italiya <dai.globalia@gmail.com>
2025-09-09 15:12:46 +0530

    Insert data in sqlite database

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "rk4hi5jthn5tji56h6kj7nkjntjgkfng"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     __tablename__ = "users"
@@ -17,20 +19,27 @@ class User(db.Model):
     def __repr__(self):
         return f"User: {self.name}, email: {self.email}"
 
+
 @app.route("/")
 def home():
     item_list = ["python", "react", "node.js", "next.js", "websocket"]
     return render_template("index.html", items=item_list)
 
+
 @app.route("/submit", methods=["GET", "POST"])
 def submitData():
     form = MyForm()
     if form.validate_on_submit():
+
+        new_user = User(name=form.name.data, email=form.email.data)
+        db.session.add(new_user)
+        db.session.commit()
+
         flash("Form submitted successfully...")
-        
+
         return redirect(url_for("submitData"))
     else:
-       return render_template("form.html", form=form)
+        return render_template("form.html", form=form)
 
 
 if __name__ == "__main":
